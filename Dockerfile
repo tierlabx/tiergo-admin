7547a6a0aece59154d4c7ff2660c76336f7e0010
tierlabx <1765080897zh@gmail.com>
2025-09-10 15:56:41 +0800

    feat: 优化viper.go 修复docker 错误

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,12 @@ FROM golang:1.24.6-alpine
 WORKDIR /app
 
 RUN go install github.com/air-verse/air@latest
+# 安装 air
+RUN apk add --no-cache git && \
+    go install github.com/air-verse/air@latest
 
+# 将 air 加入 PATH
+ENV PATH=$PATH:/go/bin
 
 COPY go.mod ./
 COPY go.sum ./
--- internal/config/viper.go
+++ internal/config/viper.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/joho/godotenv"
@@ -22,8 +23,8 @@ type DbConfig struct {
 }
 
 type WebConfig struct {
-	Host string `yaml:"host"`
-	Port string `yaml:"port"`
+	Host string `yaml:"Host"`
+	Port string `yaml:"Port"`
 }
 type Config struct {
 	DB     DbConfig  `mapstructure:"DB"`
@@ -31,47 +32,30 @@ type Config struct {
 }
 
 func (d *Config) InitConfig() {
-	// 加载 .env 环境变量文件
-	_ = godotenv.Load()
+	_ = godotenv.Load() // 忽略错误，生产可能没有 .env
 
-	viper.SetConfigFile("tier-up/config.yaml") // 指定配置文件路径
-	viper.SetConfigName("config")              // 配置文件名称(无扩展名)
-	viper.SetConfigType("yaml")                // 如果配置文件的名称中没有扩展名，则需要配置此项
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("./config")
 
-	viper.AddConfigPath(".") // 还可以在工作目录中查找配置
-	viper.WatchConfig()      //监控配置文件更新
+	// 环境变量优先
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
-	err := viper.ReadInConfig() // 查找并读取配置文件
-
-	viper.AutomaticEnv()                                   // 自动查找环境变量
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_")) // app.port → APP_PORT
-	// viper 会自动映射 同名变量
-	if err != nil { // 处理读取配置文件的错误
-		panic(fmt.Errorf("fatal error config file %v", err))
-	}
-	// 读取配置文件
-	autoCreateTable := viper.GetBool("DB.AutoCreateTable")
-	user := viper.GetString("DB.User")
-	password := viper.GetString("DB.Password")
-	host := viper.GetString("DB.Host")
-	port := viper.GetString("DB.Port")
-	driverName := viper.GetString("DB.DriverName")
-	charset := viper.GetString("DB.Charset")
-
-	d.DB = DbConfig{
-		AutoCreateTable: autoCreateTable,
-		User:            user,
-		Password:        password,
-		Port:            port,
-		Host:            host,
-		DriverName:      driverName,
-		Charset:         charset,
-	}
-	d.WebApi = WebConfig{
-		Host: viper.GetString("WebApi.Host"),
-		Port: viper.GetString("WebApi.Port"),
+	if err := viper.ReadInConfig(); err != nil {
+		panic(fmt.Errorf("配置文件没有找到: %v", err))
 	}
 
+	// 绑定配置
+	if err := viper.Unmarshal(&d); err != nil {
+		panic(fmt.Errorf("无法解码配置: %v", err))
+	}
+
+	// 优先环境变量覆盖敏感字段
+	if pwd := os.Getenv("DB_PASSWORD"); pwd != "" {
+		d.DB.Password = pwd
+	}
 }
 
 func Load() Config {
